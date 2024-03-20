955afb3ae6c757adef107762d919101c517295dd
AngeloC89 <Angelociulla89@gmail.com>
2024-03-20 20:16:42 +0100

    the game now work - make the verify

--- js/script.js
+++ js/script.js
@@ -11,68 +11,89 @@ Consigli del giorno:
 
 // generatore.addEventListener('click', funzione che genera numero);
 
+
 const printer = document.getElementById('display'); //richiama il div che contiene i quadrati a schermo.
 const starter = document.querySelector('#generate'); //button che aziona le funzioni per generate quadrati e numeri.
 const control = document.querySelector('#verify'); //button per azionare la funzione di verifica.
+const winPrint = document.getElementById('numsWin');
 const squareNum = document.getElementById('nums'); //il quadrato dove stampiamo i singoli numeri random.
-const userNum = document.getElementById('insert'); //input riceve il numero dall'utente.
+//input riceve il numero dall'utente.
+const numsWin = document.getElementById('numsWin');
+
 
 
 //button starter che genera numeri al clic.
-starter.addEventListener('click', function(){
-   printer.innerHTML = '';
+starter.addEventListener('click', function () {
+    printer.innerHTML = '';
+    const arrayNums = genNums();
+    makeResultSquare(arrayNums, printer);
+    console.log(arrayNums);
 
-    
-const arrayNums = genNums ();
-makeResultSquare(arrayNums, printer);
+   
+
+//questo bottone memorizza e verifica i numeri dell'utente
+control.addEventListener('click', function () {
+    const userNum = document.querySelectorAll('.insert');
+    const arrayUser = [];
+
+
+    for (let i = 0; i < userNum.length; i++) {
+        const num = parseInt(userNum[i].value);
+        if (!isNaN(num)) {
+            arrayUser.push(num);
+        }
+    }
+
+
+
+
+    let winsNums = [];
+
+    for (let i = 0; i < arrayNums.length; i++) {
+        if (arrayNums.includes(arrayUser[i])) {
+            winsNums.push(arrayUser[i]);
+
+        }
+    }
+    console.log(arrayUser);
+    console.log(winsNums);
+    winPrint.textContent = winsNums
 
 });
 
-control.addEventListener('click', function(){
-confront(arrayNums, )
+});
 
-})
+
 
 
 //funzioni per stampare 5 numeri random in 5 differenti quadrati, spariscono dopo 30 secondi.
-function genNums (){
+function genNums() {
 
     const arrayNums = [];
 
-    while(arrayNums.length < 5){
-        let nums = getRndInteger (1, 100);
-        if(!arrayNums.includes(nums)){
+    while (arrayNums.length < 5) {
+        let nums = getRndInteger(1, 100);
+        if (!arrayNums.includes(nums)) {
             arrayNums.push(nums)
         }
     }
     return arrayNums
-    
-    
+
+
 }
-function makeResultSquare(array, container){
+function makeResultSquare(array, container) {
 
     for (let i = 0; i < array.length; i++) {
         const div = document.createElement("div");
-        div.setAttribute('id', 'nums');
+        div.setAttribute('class', 'nums');
         div.textContent = array[i];
-        setTimeout(function(){
+        setTimeout(function () {
             div.textContent = '';
         }, 3000)
         container.appendChild(div);
-      }
+    }
 }
 
-function confront(arrayRnd, arrayUser){
-    let winsNums = [];
-
-    for(let i = 0; i < arrayRnd.length; i++){
-        if(arrayRnd.includes(arrayUser[i])){
-            winsNums.push(arrayUser);
-
-        }
-        return winsNums;
-    }
-};
 
 
 
