7e25abb714e387894de1473c545ade23c17b7cb5
AngeloC89 <Angelociulla89@gmail.com>
2024-03-21 00:21:53 +0100

    made basic HTML graphics and fixed controls

--- js/script.js
+++ js/script.js
@@ -15,53 +15,81 @@ Consigli del giorno:
 const printer = document.getElementById('display'); //richiama il div che contiene i quadrati a schermo.
 const starter = document.querySelector('#generate'); //button che aziona le funzioni per generate quadrati e numeri.
 const control = document.querySelector('#verify'); //button per azionare la funzione di verifica.
-const winPrint = document.getElementById('numsWin');
+const reset = document.querySelector('#again');//resetta il gioco.
 const squareNum = document.getElementById('nums'); //il quadrato dove stampiamo i singoli numeri random.
-//input riceve il numero dall'utente.
-const numsWin = document.getElementById('numsWin');
+const message = document.getElementById('messageWin'); 
+const happines = document.getElementById('happines'); 
 
+
+const resultDiv = document.getElementById('displayTwo')
 
 
 //button starter che genera numeri al clic.
 starter.addEventListener('click', function () {
+   
     printer.innerHTML = '';
     const arrayNums = genNums();
     makeResultSquare(arrayNums, printer);
     console.log(arrayNums);
+    const dNone = setTimeout(function () {
+        printer.classList.add('d-none');
+        starter.classList.add('d-none');
+        resultDiv.classList.remove('d-none')
+        
+    }, 30000)
+ 
 
-   
+    //questo bottone memorizza e verifica i numeri dell'utente
+    control.addEventListener('click', function () {
+        const numsWin= document.getElementById('numsWin');
+        const userNum = document.querySelectorAll('.insert');//input riceve il numero dall'utente.
+        const arrayUser = [];
+        verify.classList.add('d-none');
+        reset.classList.remove('d-none');
 
-//questo bottone memorizza e verifica i numeri dell'utente
-control.addEventListener('click', function () {
-    const userNum = document.querySelectorAll('.insert');
-    const arrayUser = [];
 
 
-    for (let i = 0; i < userNum.length; i++) {
-        const num = parseInt(userNum[i].value);
-        if (!isNaN(num)) {
-            arrayUser.push(num);
+        for (let i = 0; i < userNum.length; i++) {
+            const num = parseInt(userNum[i].value);
+            if (!isNaN(num) || (num) === 5) {
+                arrayUser.push(num);
+                if(arrayUser.length === 5){
+                    happines.textContent = ('Complimenti hai buona memoria!!!')
+                    
+                }
+               
+            }else{
+                numsWin.textContent = ('Valore inserito non valido!')
+            }      reset.classList.remove('d-none');
         }
-    }
 
 
 
 
-    let winsNums = [];
+        const winsNums = [];
 
-    for (let i = 0; i < arrayNums.length; i++) {
-        if (arrayNums.includes(arrayUser[i])) {
-            winsNums.push(arrayUser[i]);
+        for (let i = 0; i < arrayNums.length; i++) {
+            if (arrayNums.includes(arrayUser[i])) {
+                winsNums.push(arrayUser[i]);
 
+            } 
         }
-    }
-    console.log(arrayUser);
-    console.log(winsNums);
-    winPrint.textContent = winsNums
 
-});
+        
+        console.log(arrayUser);
+        console.log(winsNums);
+        
 
-});
+        message.textContent = ('I numeri estratti erano ' + arrayNums + ' hai indovinato ' + winsNums.length + ' numeri!');
+        makeResultSquare(winsNums, numsWin);
+        
+
+
+
+        
+    });
+
+},{once: true});
 
 
 
@@ -87,14 +115,9 @@ function makeResultSquare(array, container) {
         const div = document.createElement("div");
         div.setAttribute('class', 'nums');
         div.textContent = array[i];
-        setTimeout(function () {
-            div.textContent = '';
-        }, 3000)
         container.appendChild(div);
     }
 }
-
-
 
 
 
