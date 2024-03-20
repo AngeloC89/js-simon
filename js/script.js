54fabc01ca05a6b77531d4cc8494a9318efa89c6
AngeloC89 <Angelociulla89@gmail.com>
2024-03-20 15:23:19 +0100

    created function for random nums and square for nums

---
+++ js/script.js
@@ -0,0 +1,56 @@
+/*
+Visualizzare in pagina 5 numeri casuali ( tra 1 e 100) non duplicati.
+Da lì parte un timer di 30 secondi.
+Dopo i 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt() ( o meglio caselle di input).
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+Consigli del giorno:
+* Pensate prima in italiano.
+* Dividete in piccoli problemi la consegna.
+* Individuate gli elementi di cui avete bisogno per realizzare il programma.
+*/
+
+// const generatore = document.getElementById('generate');
+
+// generatore.addEventListener('click', funzione che genera numero);
+
+const stampa = document.getElementById('display');
+
+function genNums (){
+
+    const arrayNums = [];
+
+    while(arrayNums.length < 5){
+        let nums = getRndInteger (1, 100);
+        if(!arrayNums.includes(nums)){
+            arrayNums.push(nums)
+        }
+    }
+    return arrayNums
+    
+    
+}
+const arrayNums = genNums ();
+
+function makeResultSquare(array, contenitore){
+    for (let i = 0; i < array.length; i++) {
+        const div = document.createElement("div");
+        div.setAttribute('id', 'nums');
+        div.textContent = array[i];
+        contenitore.appendChild(div);
+      }
+}
+
+makeResultSquare(arrayNums, stampa);
+
+
+
+
+
+
+
+
+
+
+
+
+
